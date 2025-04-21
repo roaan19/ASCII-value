1ae400f3500c3f6363160730314e2290867c8a1e
roaan19 <141211644+roaan19@users.noreply.github.com>
2025-04-21 13:28:25 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+n=input("enter the alphabet : ")
+count=65
+if n =="A":
+    print("ASCII value of A is ",count)
+elif n=="B":
+   count=count+1
+   print("ASCII value of B is ",count )
+elif n=="C":
+  count=count+2
+  print("ASCII value of C is ",count)
+elif n=="D":
+  count=count+3
+  print("ASCII value of D is ",count)
+elif n=="E":
+  count=count+4
+  print("ASCII value of E is ",count)
+elif n=="F":
+  count=count+5
+  print("ASCII value of F is ",count)
+elif n=="G":
+  count=count+6
+  print("ASCII value of G is ",count)
+
+
